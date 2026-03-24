2d512335f0bded1de9cc994ce07c6ef0e23abeb6
Alex Robinson <alexrrobinson05@gmail.com>
2026-03-24 07:48:57 -0500

    simple databse added.

--- bot.ts
+++ bot.ts
@@ -1,6 +1,19 @@
-import { Client, Events, GatewayIntentBits } from "discord.js";
+import { 
+  Client, GatewayIntentBits, EmbedBuilder, Partials, 
+  Message, Events, ActivityType, TextChannel 
+} from 'discord.js';
+import { Database } from 'bun:sqlite';
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const db = new Database("message_cache.sqlite", { create: true });
+db.run("CREATE TABLE IF NOT EXISTS messages (id TEXT PRIMARY KEY, content TEXT, authorTag TEXT, authorId TEXT, channelId TEXT, timestamp INTEGER)");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, 
+    GatewayIntentBits.MessageContent
+  ],
+  partials: [Partials.Message, Partials.Channel],
+});
 
 client.once(Events.ClientReady, c => {
   console.log(`Ready! Logged in as ${c.user.tag}`);
