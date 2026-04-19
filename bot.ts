2ea320a2dde00f6094b4bdf1c1d011fa4b45ca53
Alex Robinson <alexrrobinson05@gmail.com>
2026-04-19 12:46:32 -0500

    added LOG_CHANNEL import from env

--- bot.ts
+++ bot.ts
@@ -10,7 +10,10 @@ import {
   TextChannel,
 } from "discord.js";
 import { Database } from "bun:sqlite";
+
 const TOKEN = process.env.DISCORD_TOKEN;
+const LOG_CHANNEL = process.env.LOG_CHANNEL;
+
 const db = new Database("message_cache.sqlite", { create: true });
 db.run("CREATE TABLE IF NOT EXISTS messages (id TEXT PRIMARY KEY, content TEXT, authorTag TEXT, authorId TEXT, channelId TEXT, timestamp INTEGER)");
 
@@ -25,6 +28,7 @@ const client = new Client({
 
 client.once(Events.ClientReady, async (c) => {
   console.log("Ready");
+  console.log(LOG_CHANNEL)
   c.user.setActivity("Hey people");
 });
 
